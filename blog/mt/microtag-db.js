398a192e07daca80d01e848b41daba0cf1d71a9d
anon <anon>
2026-04-20 21:59:13 -0300

    directory structure change

---
+++ blog/mt/microtag-db.js
@@ -0,0 +1,17 @@
+export const DB = {
+  info: {
+    updated_at: "Mon Apr 20 09:31:48 PM -03 2026",
+    homeMsg: "Esta é uma versão modificada do <a href='https://github.com/odetra/microtag' target='_blank'>Microtag</a> que integrei no blog para organizar os arquivos que eu uso aqui.<br><br>",
+    storageMsg: "Currently serving 9 files. Using 228K of disk space. ",
+    tags: ["flower", "hd-bay", "homepage", "neet", "neet_army", "screenshot", "t420", "thinkpad", "trackpoint", "webp", ],
+  },
+  files: [
+    { md5: "ba449d96b04e892a38fc8a42f2957f25", ext: "webp", filename:    "null", filesize: "32K", timestamp:    "2026-04-20 19:50:47", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "neet", "neet_army", "homepage", "webp", ] },
+{ md5: "1173d1a454bf27fc5b5a9f4449002193", ext: "webp", filename:    "null", filesize: "28K", timestamp:    "2026-04-15 21:29:23", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "thinkpad", "trackpoint", "webp", ] },
+{ md5: "11a4989fcdae1c98913fb997307eb401", ext: "webp", filename:    "null", filesize: "20K", timestamp:    "2026-04-15 21:29:23", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "screenshot", "thinkpad", "webp", ] },
+{ md5: "5eebab28095ae53fe12444fd519dbd65", ext: "webp", filename:    "null", filesize: "4.0K", timestamp:    "2026-04-15 21:29:23", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "thinkpad", "webp", ] },
+{ md5: "cc8b87bf6b853875edb83a6bfef7b9b5", ext: "webp", filename:    "null", filesize: "56K", timestamp:    "2026-04-15 21:29:23", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "thinkpad", "t420", "webp", ] },
+{ md5: "074db0d84671409cd1ecb064d9967656", ext: "webp", filename:    "null", filesize: "16K", timestamp:    "2026-04-15 21:29:23", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "hd-bay", "webp", ] },
+{ md5: "576eb493902de9f470cd8e696c3f0c50", ext: "webp", filename:    "null", filesize: "48K", timestamp:    "2026-04-15 20:51:43", title: "Untitled.", commentary:     "No commentary.", tags: [ "*", "neet", "flower", "webp", ] },
+  ]
+};
